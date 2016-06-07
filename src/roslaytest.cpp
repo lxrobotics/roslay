13d9750effb014e09628ba155d689e2cb6dad075
Alexander Entinger <cto@lxrobotics.com>
2016-06-07 14:11:02 +0200

    Finishing implementation of roslaytest

--- src/roslaytest.cpp
+++ src/roslaytest.cpp
@@ -8,12 +8,13 @@
 
 #include <ros/ros.h>
 
-#include <std_msgs/Bool.h>
+#include <roslay/Relay.h>
 
 /**************************************************************************************
  * PROTOTYPES
  **************************************************************************************/
 
+void handleSetRelay             (ros::Publisher &relay_publisher);
 void handleExit                 ();
 void handleInvalidValue         ();
 
@@ -29,7 +30,7 @@ int main(int argc, char **argv)
 
   /* Create the publisher for sending relay messages to the arduino */
   
-  ros::Publisher relay_publisher = node_handle.advertise<std_msgs::Bool>("/relay", 10);
+  ros::Publisher relay_publisher = node_handle.advertise<roslay::Relay>("/relay", 10);
 
   /* Provide a crude menu for selecting which service one wants to invoke */
 
@@ -38,14 +39,16 @@ int main(int argc, char **argv)
   do
   {
     std::cout << std::endl;
+    std::cout << "[s]et relay" << std::endl;
     std::cout << "[q]uit" << std::endl;
 
     std::cout << ">>"; std::cin >> cmd;
 
     switch(cmd)
     {
-    case 'q': handleExit                 ();                       break;
-    default:  handleInvalidValue         ();                       break;
+    case 's': handleSetRelay    (relay_publisher);  break;
+    case 'q': handleExit        ();                 break;
+    default:  handleInvalidValue();                 break;
     }
   } while(cmd != 'q');
 
@@ -56,9 +59,26 @@ int main(int argc, char **argv)
  * OUR FUNCTIONS
  **************************************************************************************/
 
+void handleSetRelay(ros::Publisher &relay_publisher)
+{
+  std::cout << "Enter relay number (0 - 3):";
+  size_t relay_number;
+  std::cin >> relay_number;
+  std::cout << "turn relay o[n]" << std::endl;
+  std::cout << "turn relay of[f]" << std::endl;
+  char relay_cmd = 0;
+  std::cout << ">> "; std::cin >> relay_cmd;
+  
+  roslay::Relay msg;
+  msg.relay = static_cast<uint8_t>(relay_number);
+  msg.turn_on = relay_cmd == 'n';
+
+  relay_publisher.publish(msg);
+}
+
 void handleExit()
 {
-  std::cout << "Exiting function ardumotetest" << std::endl;
+  std::cout << "Exiting function roslaytest" << std::endl;
 }
 
 void handleInvalidValue()
